223ca27c8a48e5869ff9d3b72b627112eabea86f
Signe Johansson <signe.johansson.5006@student.lu.se>
2024-02-02 17:49:25 +0100

    the bee image will now move when phone is tilted, it is possible to move it with or without using the animations

--- app/src/main/java/com/example/hellosensor/activity_2.java
+++ app/src/main/java/com/example/hellosensor/activity_2.java
@@ -2,7 +2,9 @@ package com.example.hellosensor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
 import android.content.Context;
+import android.graphics.Point;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -10,6 +12,14 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.media.MediaPlayer;
 import android.os.Vibrator;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 
 public class activity_2 extends AppCompatActivity implements SensorEventListener {
@@ -23,6 +33,12 @@ public class activity_2 extends AppCompatActivity implements SensorEventListener
     private static final int SHAKE_THRESHOLD = 600;
     private Vibrator vibrator;
     private MediaPlayer mediaPlayer;
+    private ImageView imageView;
+    private float[] gravity = new float[3];
+    private float[] linear_acceleration = new float[3];
+    final float alpha = 0.8f;
+    private boolean isAnimationRunning = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +50,7 @@ public class activity_2 extends AppCompatActivity implements SensorEventListener
         senSensorManager.registerListener(this, senAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         mediaPlayer = MediaPlayer.create(this, R.raw.bee); //sound effect
+        imageView = findViewById(R.id.imageView); //bee image
     }
 
     @Override
@@ -45,7 +62,6 @@ public class activity_2 extends AppCompatActivity implements SensorEventListener
         super.onDestroy();
     }
 
-    //detects a shake gesture
     @Override
     public void onSensorChanged(SensorEvent event) {
         Sensor mySensor = event.sensor;
@@ -54,6 +70,41 @@ public class activity_2 extends AppCompatActivity implements SensorEventListener
             float y = event.values[1];
             float z = event.values[2];
 
+            //move bee without using animations
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * x;
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * y;
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * z;
+
+            linear_acceleration[0] = event.values[0] - gravity[0];
+            linear_acceleration[1] = event.values[1] - gravity[1];
+            linear_acceleration[2] = event.values[2] - gravity[2];
+
+            //update position
+            float tiltX = linear_acceleration[0];
+            float tiltY = linear_acceleration[1];
+            float tiltZ = linear_acceleration[2];
+
+            //move image as phone is tilted
+            float tiltThreshold = 1.5f;
+            if (Math.abs(tiltX) > tiltThreshold || Math.abs(tiltY) > tiltThreshold || Math.abs(tiltZ) > tiltThreshold) {
+                float multiplier = 20f;
+                //translation values
+                float translationX = -tiltX * multiplier;
+                float translationY = tiltY * multiplier;
+                float translationZ = tiltZ * multiplier;
+
+                //new position
+                float newX = imageView.getX() + translationX;
+                float newY = imageView.getY() + translationY;
+                float newZ = imageView.getZ() + translationZ;
+
+                //update position of imageview
+                imageView.setX(newX);
+                imageView.setY(newY);
+                imageView.setZ(newZ);
+            }
+
+            //vibrate and play sound if phone is shaken
             long curTime = System.currentTimeMillis();
             if ((curTime - lastUpdate) > 100) {
                 long diffTime = (curTime - lastUpdate);
@@ -72,6 +123,47 @@ public class activity_2 extends AppCompatActivity implements SensorEventListener
                 last_y = y;
                 last_z = z;
             }
+
+            /*
+            //move bee using animations
+            gravity[0] = alpha * gravity[0] + (1 - alpha) * x;
+            gravity[1] = alpha * gravity[1] + (1 - alpha) * y;
+            gravity[2] = alpha * gravity[2] + (1 - alpha) * z;
+
+            linear_acceleration[0] = event.values[0] - gravity[0];
+            linear_acceleration[1] = event.values[1] - gravity[1];
+            linear_acceleration[2] = event.values[2] - gravity[2];
+
+            float tiltX = linear_acceleration[0];
+            float tiltY = linear_acceleration[1];
+            float tiltZ = linear_acceleration[2];
+
+            //start animation when phone is tilted
+            float tiltThreshold = 3.0f;
+            if (Math.abs(tiltX) > tiltThreshold) {
+                if(tiltX < 0) {
+                    startAnimation(R.anim.move_right);
+                }
+                else if (tiltX > 0) {
+                    startAnimation(R.anim.move_left);
+                }
+            }
+            if(Math.abs(tiltY) > tiltThreshold) {
+                if(tiltY < 0) {
+                    startAnimation(R.anim.move_up);
+                }
+                else if(tiltY > 0) {
+                    startAnimation(R.anim.move_down);
+                }
+            }
+            if(Math.abs(tiltZ) > tiltThreshold) {
+                if(tiltZ < 0) {
+                    startAnimation(R.anim.move_down);
+                }
+                else if(tiltZ > 0) {
+                    startAnimation(R.anim.move_up);
+                }
+            } */
         }
     }
 
@@ -90,5 +182,25 @@ public class activity_2 extends AppCompatActivity implements SensorEventListener
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
+
+    /*
+    private void startAnimation(int animationResource) {
+        if (!isAnimationRunning) {
+            isAnimationRunning = true;
+            Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), animationResource);
+            imageView.startAnimation(animation);
+            animation.setAnimationListener(new Animation.AnimationListener() {
+                @Override
+                public void onAnimationStart(Animation animation) {}
+
+                @Override
+                public void onAnimationEnd(Animation animation) {
+                    isAnimationRunning = false;
+                }
+                @Override
+                public void onAnimationRepeat(Animation animation) {}
+            });
+        }
+    }*/
 }
 
